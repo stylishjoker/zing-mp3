42275c9eea7ff3e1ddc059bbdbb2ea0f94b96864
stylishjoker <animetplink@gmail.com>
2023-07-11 09:06:26 +0700

    handle silder

--- components/slick-slider/index.tsx
+++ components/slick-slider/index.tsx
@@ -12,66 +12,68 @@ const SlickSlider: FC = () => {
 	const banner = useSelector((state: RootState) => state.appSlice.banner);
 	useEffect(() => {
 		dispacth(actions.getHomeAtions());
-		const sliderEls = document.getElementsByClassName(
-			'slider-item'
-		) as HTMLCollectionOf<HTMLElement>;
-		let min = 0;
-		let max = 2;
-		const intervalId = setInterval(() => {
-			const list = getArrSlider(min, max, sliderEls.length - 1);
-			for (let i = 0; i < sliderEls.length; i++) {
-				// Delete classnames (css)
-				sliderEls[i]?.classList?.remove(
-					'animate-slide-right',
-					'order-last',
-					'z-20'
-				);
-				sliderEls[i]?.classList?.remove(
-					'animate-slide-left',
-					'order-first',
-					'z-10'
-				);
-				sliderEls[i]?.classList?.remove(
-					'animate-slide-left2',
-					'order-2',
-					'z-10'
-				);
-
-				// Hide or Show images
-				if (list.some((item) => item === i)) {
-					sliderEls[i].style.cssText = `display: block`;
-				} else {
-					sliderEls[i].style.cssText = `display: none`;
-				}
-			}
-			// Add animation by adding classnames
-			list.forEach((item) => {
-				if (item === max) {
-					sliderEls[item]?.classList?.add(
+		if (banner.length > 0) {
+			const sliderEls = document.getElementsByClassName(
+				'slider-item'
+			) as HTMLCollectionOf<HTMLElement>;
+			let min = 0;
+			let max = 2;
+			const intervalId = setInterval(() => {
+				const list = getArrSlider(min, max, sliderEls.length - 1);
+				for (let i = 0; i < sliderEls.length; i++) {
+					// Delete classnames (css)
+					sliderEls[i]?.classList?.remove(
 						'animate-slide-right',
 						'order-last',
 						'z-20'
 					);
-				} else if (item === min) {
-					sliderEls[item]?.classList?.add(
+					sliderEls[i]?.classList?.remove(
 						'animate-slide-left',
 						'order-first',
 						'z-10'
 					);
-				} else {
-					sliderEls[item]?.classList?.add(
+					sliderEls[i]?.classList?.remove(
 						'animate-slide-left2',
 						'order-2',
 						'z-10'
 					);
+
+					// Hide or Show images
+					if (list.some((item) => item === i)) {
+						sliderEls[i].style.cssText = `display: block`;
+					} else {
+						sliderEls[i].style.cssText = `display: none`;
+					}
 				}
-			});
-			min = min === sliderEls.length - 1 ? 0 : min + 1;
-			max = max === sliderEls.length - 1 ? 0 : max + 1;
-		}, 3000);
-		return () => {
-			intervalId && clearInterval(intervalId);
-		};
+				// Add animation by adding classnames
+				list.forEach((item) => {
+					if (item === max) {
+						sliderEls[item]?.classList?.add(
+							'animate-slide-right',
+							'order-last',
+							'z-20'
+						);
+					} else if (item === min) {
+						sliderEls[item]?.classList?.add(
+							'animate-slide-left',
+							'order-first',
+							'z-10'
+						);
+					} else {
+						sliderEls[item]?.classList?.add(
+							'animate-slide-left2',
+							'order-2',
+							'z-10'
+						);
+					}
+				});
+				min = min === sliderEls.length - 1 ? 0 : min + 1;
+				max = max === sliderEls.length - 1 ? 0 : max + 1;
+			}, 3000);
+			return () => {
+				intervalId && clearInterval(intervalId);
+			};
+		}
 	}, []);
 
 	return (
